be8e74ecf9979d7da85791842ec88056424618e2
Macintosh <macintosh@Macintosh-MacBook-Air.local>
2019-03-10 17:26:38 +0100

    delete feature update

--- scripts.js
+++ scripts.js
@@ -4,29 +4,37 @@
 
 let id = 0;
 
+function blogpost(post){
+  $(".left-side").append(
+  `
+  <article>
+    <!--- <img src="roka.jpg" height=300px>--->
+    <div class="article-content">
+      <h2>${post.title}</h2>
+      <p>${post.text}</p>
+      <button type="button" data-delete="${post.id}" class="delete-button"> X </button>
+    </div>
+  </article>
+  `
+);
+}
+
 $(".newpost").on("click", (event) => {
  event.preventDefault();
    let newTitle = $(".title").val();
    let newContent = $(".content2").val();
-  $(".left-side").append(
-    `
-    <article>
-      <!--- <img src="roka.jpg" height=300px>--->
-      <div class="article-content">
-        <h2> ${newTitle} </h2>
-        <p> ${newContent} </p>
-      </div>
-    </article>
-    `)
     id = id + 1;
     let path = "posts/" + id;
 
     let dataToSave = {
+      id: id,
       title: newTitle,
       text: newContent
     };
-    
-    fb.ref(path).set(dataToSave);
+
+    fb.ref(path).set(dataToSave).then(function(){
+      blogpost(dataToSave)
+    })
     $(".title").val("");
     $(".content2").val("");
   }
@@ -49,7 +57,7 @@ initialData.forEach( post => {
     <div class="article-content">
       <h2>${post.title}</h2>
       <p>${post.text}</p>
-      <!---<button type="button" data-delete="1" class="delete-button"> X </button>--->
+      <button type="button" data-delete="${post.id}" class="delete-button"> X </button>
     </div>
   </article>
   `
@@ -62,36 +70,32 @@ fb.ref("posts").once('value').then(data => {
   let initialData = data.val();
   if (initialData !== null) {
     initialData.forEach( post => {
-      $(".left-side").append(
-      `
-      <article>
-        <!--- <img src="roka.jpg" height=300px>--->
-        <div class="article-content">
-          <h2>${post.title}</h2>
-          <p>${post.text}</p>
-          <!---<button type="button" data-delete="1" class="delete-button"> X </button>--->
-        </div>
-      </article>
-      `
-    );
+      console.log(post);
       id = initialData.length - 1;
     })
 }});
 
 // delete button
 
-// fb.ref("posts/2").remove();
+//fb.ref("posts/2").remove();
 
 
 $(".left-side").on("click", ".delete-button", (event) => {
-    fb.ref("posts/" + $(event.target).attr("data-delete")).remove();
+    if (confirm("Biztosan törlöd?")){
+      let deletingid = $(event.target).attr("data-delete")
+      fb.ref("posts/" + deletingid).remove(function (error){
+        //ennek a segítségével frissítés nélkül törlődik a poszt
+        $(event.target).parent().parent().remove();
+      });
+      console.log(deletingid)
+    }
 });
 
 
 
 // firebase update
 
-/*let samePath = "posts/1";
+let samePath = "posts/1";
 let updatedData = {
   title: "My first edited and updated blog post",
   text: "Some hilarious content again, which proves how awesome I am again."
@@ -100,7 +104,7 @@ fb.ref(samePath).set(updatedData);
 
 // firebase delete
 
-fb.ref("posts/2").remove(); */
+//fb.ref("posts/2").remove();
 
 /* firebase write
 
